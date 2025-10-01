17dd8cfc4f479ad48dcfdd7e69b6445e9673f01f
Petro <petrobokalo1@gmail.com>
2025-10-01 21:31:32 +0300

    Connected back to register user

---
+++ js/auth/register.js
@@ -0,0 +1,40 @@
+document.getElementById("signupForm").addEventListener("submit", async function (event) {
+    event.preventDefault(); // блокує стандартне оновлення сторінки
+
+    const password = document.getElementById("signupPassword").value;
+    const confirmPassword = document.getElementById("signupConfirmPassword").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    const data = {
+        userFirstName: document.getElementById("signupFirstName").value,
+        userLastName: document.getElementById("signupLastName").value,
+        email: document.getElementById("signupEmail").value,
+        phoneNumber: document.getElementById("signupPhone").value,
+        password: password
+    };
+
+    try {
+        const response = await fetch("https://localhost:7012/api/Auth/register", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(data)
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            alert(result.message);
+            console.log(result);
+        } else {
+            alert(result.message || "Registration failed");
+            console.error(result);
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Server error");
+    }
+});
